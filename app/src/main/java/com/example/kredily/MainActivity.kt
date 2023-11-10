c9e506e15fce95ea510c840fd225232cc045d339
Pranit Rane <pranitrane619@gmail.com>
2023-11-11 03:38:59 +0530

    update: app style updated
    1. colors updated
    2. dimens added
    3. styles added
    4. splash changes
    5. main activity theme changes

--- app/src/main/java/com/example/kredily/MainActivity.kt
+++ app/src/main/java/com/example/kredily/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.kredily
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.databinding.DataBindingUtil
+import com.example.kredily.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Global
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val colorBackground = resources.getColor(R.color.color_background, null)
+            window.statusBarColor = colorBackground
+            window.navigationBarColor = colorBackground
+            val isNightMode =
+                when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                    Configuration.UI_MODE_NIGHT_YES -> true
+                    else -> false
+                }
+            if (!isNightMode) {
+                val decorView = window.decorView
+                val controller = WindowInsetsControllerCompat(window, decorView)
+                controller.isAppearanceLightStatusBars = true
+                controller.isAppearanceLightNavigationBars = true
+            }
+        }
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
