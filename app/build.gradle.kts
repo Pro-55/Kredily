f8ec85f68a3de0ecebfebc06f4bcff1d45cd5139
Pranit Rane <pranitrane619@gmail.com>
2023-11-10 20:36:53 +0530

    update: added dependencies
    1. recycler view
    2. fragment
    3. navigation
    4. lifecycle
    5. hilt
    6. coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.parcelize")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -61,6 +65,31 @@ dependencies {
 
     // Constraint Layout
     implementation(libs.androidx.constraintlayout)
+
+    // RecyclerView
+    implementation(libs.androidx.recyclerview)
+
+    // Architecture Components Lifecycle Extensions
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.common.java8)
+
+    // Fragment
+    implementation(libs.androidx.fragment)
+
+    // Navigation Component
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Hilt DI
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     // Test
     testImplementation(libs.junit)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@
 plugins {
     id("com.android.application") version libs.versions.androidGradlePlugin.get() apply false
     kotlin("android") version libs.versions.kotlin.get() apply false
+    id("com.google.devtools.ksp") version libs.versions.ksp.get() apply false
+    id("androidx.navigation.safeargs") version libs.versions.navigationSafeArgs apply false
+    id("com.google.dagger.hilt.android") version libs.versions.hilt.get() apply false
 }
 
 tasks.register("clean", Delete::class) {
